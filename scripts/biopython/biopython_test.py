e2943e077d4bcf4e5f4a8c0c8b86fb65f6687aec
Parsa Fouladi <parsa_fouladi@yahoo.com>
2024-03-19 14:17:43 +0000

    update: basic "KAIEP" search is added to biopython file

--- scripts/biopython/biopython_test.py
+++ scripts/biopython/biopython_test.py
@@ -5,6 +5,12 @@ parser = PDBParser()
 
 # Parse the PDB file
 structure = parser.get_structure("protein", "AF-A0A087XJA9-F1-model_v4.pdb")
+ppb = PPBuilder()
+pep=ppb.build_peptides(structure)
+seq = pep[0].get_sequence()
+
+k_index=seq.find("KAIEP")
+starting_index=k_index-7
 
 # Iterate over atoms and retrieve B-factors
 red_atoms = []
@@ -21,6 +27,13 @@ for model in structure:
                     red_atoms.append(atom)
 
 class RedSelect(Select):
+    # Accept the risudes with index of higher than starting index and atoms with bfactor higher than threshold
+    def accept_residue(self, residue):
+        if residue.get_id()[1] > starting_index:
+            return 1
+        else:
+            return 0
+    
     def accept_atom(self, atom):
         if atom in red_atoms:
             return 1
@@ -31,5 +44,7 @@ class RedSelect(Select):
 io = PDBIO()
 io.set_structure(structure)
 io.save("red_only.pdb", RedSelect())
+
+print(structure)
 
 
