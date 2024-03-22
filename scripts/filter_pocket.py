8a7351f8bb9476ca31d67a820c29d3c1224cf4f3
Parsa Fouladi <parsa_fouladi@yahoo.com>
2024-03-22 15:24:01 +0000

    update: script now get input from the terminal

--- scripts/filter_pocket.py
+++ scripts/filter_pocket.py
@@ -1,9 +1,25 @@
 import sys 
 sys.path.append('..')
+import os
 from helpers.binding_pocket import BindingPocket
+from os import listdir
+from os.path import isfile, join
+
+# Check if the user provided an argument
+if len(sys.argv) > 1:
+    # The first command-line argument (index 0) is the script name
+    # So the actual argument provided by the user is at index 1
+    user_input = sys.argv[1]
+    #print("You provided input:", user_input)
+    mypath = user_input
+    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    print(onlyfiles)
 
 
-#create a binding pocket object
-binding_pocket=BindingPocket(file_path="data\AF-A0A087XJA9-F1-model_v4.pdb",output_path="data\\test_class.pdb",pocket="KAIEP",b_factor_threshold=50,num_atoms_before=7)
-#find the binding pocket
-binding_pocket.find_binding_pocket()
+    #create a binding pocket object
+    binding_pocket=BindingPocket(file_path="data\AF-A0A087XJA9-F1-model_v4.pdb",output_path="data\\test_class.pdb",pocket="KAIEP",b_factor_threshold=50,num_atoms_before=7)
+    #find the binding pocket
+    binding_pocket.find_binding_pocket()
+else:
+    print("No input provided.")
+
