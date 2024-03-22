2292517d5cd7257dbbd81689ef6fe2e5f47762e3
Parsa Fouladi <parsa_fouladi@yahoo.com>
2024-03-22 15:31:46 +0000

    update: gitignore and filter script updated

--- scripts/filter_pocket.py
+++ scripts/filter_pocket.py
@@ -13,13 +13,18 @@ if len(sys.argv) > 1:
     #print("You provided input:", user_input)
     mypath = user_input
     onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    print(onlyfiles)
+    #print(onlyfiles)
 
+    for file in onlyfiles:
+        #create a binding pocket object
+        binding_pocket=BindingPocket(file_path=mypath+'\\'+file,output_path="filter_output\\"+file,pocket="KAIEP",b_factor_threshold=50,num_atoms_before=7)
+        #find the binding pocket
+        binding_pocket.find_binding_pocket()
 
     #create a binding pocket object
-    binding_pocket=BindingPocket(file_path="data\AF-A0A087XJA9-F1-model_v4.pdb",output_path="data\\test_class.pdb",pocket="KAIEP",b_factor_threshold=50,num_atoms_before=7)
-    #find the binding pocket
-    binding_pocket.find_binding_pocket()
+    # binding_pocket=BindingPocket(file_path="data\AF-A0A087XJA9-F1-model_v4.pdb",output_path="data\\test_class.pdb",pocket="KAIEP",b_factor_threshold=50,num_atoms_before=7)
+    # #find the binding pocket
+    # binding_pocket.find_binding_pocket()
 else:
     print("No input provided.")
 
