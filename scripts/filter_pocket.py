fc98f4713f8b268aa58340e54a0c9e69c4838849
Parsa Fouladi <parsa_fouladi@yahoo.com>
2024-03-22 01:14:31 +0000

    add: the sctipt is added

---
+++ scripts/filter_pocket.py
@@ -0,0 +1,7 @@
+from helpers.binding_pocket import BindingPocket
+
+
+#create a binding pocket object
+binding_pocket=BindingPocket(file_path="data\AF-A0A087XJA9-F1-model_v4.pdb",output_path="data\\test_class.pdb",pocket="KAIEP",b_factor_threshold=50,num_atoms_before=7)
+#find the binding pocket
+binding_pocket.find_binding_pocket()
