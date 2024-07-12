36320019e3c5344b8a7b41549cb4ab99c7896e33
Parsa Fouladi <parsa_fouladi@yahoo.com>
2024-07-12 18:22:56 +0100

    update: gpu test updated

--- modelling/gpu_tests/gpu_test2.py
+++ modelling/gpu_tests/gpu_test2.py
@@ -1,3 +1,19 @@
 import torch
-for i in range(torch.cuda.device_count()):
-   print(torch.cuda.get_device_properties(i).name)
+import time
+import logging
+
+# Configure logging to write to a text file
+logging.basicConfig(filename='gpu_test_log.log', level=logging.INFO, 
+                    format='%(asctime)s %(levelname)s:%(message)s')
+
+
+try:
+
+   for i in range(torch.cuda.device_count()):
+      print(torch.cuda.get_device_properties(i).name)
+      logging.info(f"Device {i}: {torch.cuda.get_device_properties(i).name}")
+
+except Exception as e:
+   logging.error(f"Error in getting device properties: {e}")
+   print("Error in getting device properties")
+   exit(1)
