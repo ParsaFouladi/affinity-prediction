e1a3c4ca5a7b6759116677a268ad7d66b4433429
Parsa Fouladi <parsa_fouladi@yahoo.com>
2024-07-13 03:12:37 +0100

    update: dataloader updated with train-validation seperation logic

--- modelling/dataloader.py
+++ modelling/dataloader.py
@@ -1,7 +1,8 @@
 import h5py
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader,SubsetRandomSampler,Subset
 import random
+import numpy as np
 
 class ProteinLigandDataset(Dataset):
     def __init__(self, h5_file_path, transform=None):
@@ -36,12 +37,39 @@ class ProteinLigandDataset(Dataset):
 
     def close(self):
         self.h5_file.close()
+    
+    def get_train_val_split(self, val_split=0.1, seed=42):
+        """Splits the dataset into training and validation sets with shuffling."""
+        dataset_size = len(self)
+        indices = list(range(dataset_size))
+        np.random.seed(seed)  # Set random seed for reproducibility
+        np.random.shuffle(indices)
+        split = int(np.floor(val_split * dataset_size))
+        train_indices, val_indices = indices[split:], indices[:split]
 
-# test the dataloader
-path="../representation/representations.h5"
-dataset = ProteinLigandDataset(path)
-print(dataset.head())
-#print(dataset.show_random_sample())
-print(len(dataset))
-print(dataset[0])
-dataset.close()
+        train_sampler = SubsetRandomSampler(train_indices)
+        val_sampler = SubsetRandomSampler(val_indices)  # Create a sampler for validation as well
+        return train_sampler, val_sampler
+
+
+# Example Usage (Slightly modified)
+
+### Usage Example
+# dataset = ProteinLigandDataset(h5_file_path)
+
+# # Split dataset
+# train_sampler, val_sampler = dataset.get_train_val_split(dataset)
+
+# # Create DataLoaders with samplers for both training and validation
+# train_loader = DataLoader(dataset, batch_size=32, sampler=train_sampler)
+# val_loader = DataLoader(dataset, batch_size=32, sampler=val_sampler)  # Shuffle validation set
+
+# # test the dataloader
+# path="../representation/representations.h5"
+# dataset = ProteinLigandDataset(path)
+# print(dataset.head())
+# #print(dataset.show_random_sample())
+# print(len(dataset))
+# print(dataset[0])
+# print(dataset[0][0][3,1,0])
+# dataset.close()
