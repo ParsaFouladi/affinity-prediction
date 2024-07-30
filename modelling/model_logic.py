64b8282173cb4b05801b262b9e982bf5fe8d990e
Parsa Fouladi <parsa_fouladi@yahoo.com>
2024-07-30 04:51:22 +0100

    update: another model logic added

--- modelling/model_logic.py
+++ modelling/model_logic.py
@@ -75,49 +75,50 @@ class CNNModelBasic(nn.Module):
 # # Print the model architecture
 # print(model)
 
-
-class BasicBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_channels != out_channels:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
 class DeeperCNNModel(nn.Module):
     def __init__(self, input_shape):
         super(DeeperCNNModel, self).__init__()
-        self.in_channels = 64
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(64, 2, stride=2)
-        self.layer2 = self._make_layer(128, 2, stride=2)
-        self.layer3 = self._make_layer(256, 2, stride=2)
-        self.layer4 = self._make_layer(512, 2, stride=2)
-        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512, 1)
 
+        # First Convolutional Block
+        self.conv1 = self._make_layer(3, 64, 2)
+        
+        # Second Convolutional Block
+        self.conv2 = self._make_layer(64, 128, 2)
+        
+        # Third Convolutional Block
+        self.conv3 = self._make_layer(128, 256, 3)
+        
+        # Fourth Convolutional Block
+        self.conv4 = self._make_layer(256, 512, 3)
+        
+        # Fifth Convolutional Block
+        self.conv5 = self._make_layer(512, 512, 3)
+        
+        # Sixth Convolutional Block
+        self.conv6 = self._make_layer(512, 1024, 2)
+
+        # Calculate the flattened size for the fully connected layer
         self._initialize_fc(input_shape)
 
-    def _make_layer(self, out_channels, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
+        # Fully Connected Layers
+        self.fc1 = nn.Linear(self.flattened_size, 1024)
+        self.bn_fc1 = nn.BatchNorm1d(1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.bn_fc2 = nn.BatchNorm1d(512)
+        self.fc3 = nn.Linear(512, 256)
+        self.bn_fc3 = nn.BatchNorm1d(256)
+        self.dropout = nn.Dropout(0.5)
+        self.fc4 = nn.Linear(256, 1)
+
+    def _make_layer(self, in_channels, out_channels, num_blocks):
         layers = []
-        for stride in strides:
-            layers.append(BasicBlock(self.in_channels, out_channels, stride))
-            self.in_channels = out_channels
+        for _ in range(num_blocks):
+            layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1))
+            layers.append(nn.BatchNorm2d(out_channels))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout2d(0.5))  # Adding dropout to convolutional layers
+            in_channels = out_channels
+        layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
         return nn.Sequential(*layers)
 
     def _initialize_fc(self, input_shape):
@@ -127,16 +128,26 @@ class DeeperCNNModel(nn.Module):
             self.flattened_size = dummy_output.view(1, -1).size(1)
 
     def forward_conv(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
         return x
 
     def forward(self, x):
+        # Pass through convolutional layers
         x = self.forward_conv(x)
-        x = self.avg_pool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
+        
+        # Flatten the output
+        x = x.view(-1, self.flattened_size)
+        
+        # Fully Connected Layers
+        x = F.relu(self.bn_fc1(self.fc1(x)))
+        x = F.relu(self.bn_fc2(self.fc2(x)))
+        x = F.relu(self.bn_fc3(self.fc3(x)))
+        x = self.dropout(x)
+        x = self.fc4(x)  # No activation function here for regression output
+        
         return x
