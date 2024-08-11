50e403638da7368273b342871efb377796273475
Parsa Fouladi <parsa_fouladi@yahoo.com>
2024-08-11 17:58:00 +0100

    update: minor change to the scheduler

--- modelling/training.py
+++ modelling/training.py
@@ -109,7 +109,7 @@ def main(args):
     criterion = nn.MSELoss()  
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, patience=14, verbose=True,min_lr=4e-5)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, patience=16, verbose=True,min_lr=4e-5)
 
     # TensorBoard for Logging
     writer = SummaryWriter(log_dir=args.log_dir)
