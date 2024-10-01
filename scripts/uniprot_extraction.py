4cafc772cebc69dfc7799815e99c6b24f51af3d3
Parsa Fouladi <parsa_fouladi@yahoo.com>
2024-10-01 04:15:10 +0100

    update: updated the uniprot extraction

--- scripts/uniprot_extraction.py
+++ scripts/uniprot_extraction.py
@@ -8,8 +8,9 @@ import pandas as pd
 import requests
 
 # Read the CSV file into a Pandas DataFrame
-df = pd.read_csv('uniprot.csv', header=0)
-
+df = pd.read_excel('uniprot.xlsx')
+# add a new column to the DataFrame that checks if the alhaFold model is available
+df['AlphaFold'] = False
 # Loop through the DataFrames
 for i in range(len(df)):
 
@@ -19,8 +20,14 @@ for i in range(len(df)):
     url = "https://alphafold.ebi.ac.uk/files/" + filename
     # Fetch the file from the URL
     response = requests.get(url)
+    if response.status_code == 200:
+        #Update the AlhaFold column to True
+        df.iloc[i, 7] = True
 
-    # Save the file in the subfolder
-    with open('PDB_files/' + filename , 'wb') as f:
-        print("downloading ... " + url)
-        f.write(response.content)
+        # Save the file in the subfolder
+        with open('PDB_files/' + filename , 'wb') as f:
+            print("downloading ... " + url)
+            f.write(response.content)
+
+# Save the updated DataFrame to a new excel file
+df.to_excel('uniprot_with_alphafold.xlsx', index=False)
