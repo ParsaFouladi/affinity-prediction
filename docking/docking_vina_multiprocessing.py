ed4f190a0c8a5a66d5ce615909f6079ae6656523
Parsa Fouladi <parsa_fouladi@yahoo.com>
2024-11-11 16:14:18 +0000

    update: comprehensive options added for the docking script

--- docking/docking_vina_multiprocessing.py
+++ docking/docking_vina_multiprocessing.py
@@ -8,7 +8,7 @@ import datetime
 
 
 def dock_pair(pair):
-    receptor_file, ligand_file, output_folder,exhaustiveness = pair
+    receptor_file, ligand_file, output_folder,exhaustiveness,centre,box_size = pair
     try:
         receptor_name = os.path.splitext(os.path.basename(receptor_file))[0]
         ligand_name = os.path.splitext(os.path.basename(ligand_file))[0]
@@ -16,7 +16,8 @@ def dock_pair(pair):
         v = Vina(sf_name='vina')
         v.set_receptor(receptor_file)
         v.set_ligand_from_file(ligand_file)
-        v.compute_vina_maps(center=[5, 2, -15], box_size=[20, 20, 20])
+        # v.compute_vina_maps(center=[5, 2, -15], box_size=[20, 20, 20])
+        v.compute_vina_maps(center=centre, box_size=box_size)
         
         v.dock(exhaustiveness=exhaustiveness, n_poses=20)
         output_filename = '{}_{}_vina_out.pdbqt'.format(receptor_name, ligand_name)
@@ -42,6 +43,9 @@ def main(args):
     ligand_folder = args.ligand_folder
     output_folder = args.output_folder
     exhaustiveness = args.exhaustiveness
+    centre=args.center
+    box_size=args.box_size
+    num_cpus=args.num_cpus
 
     # Get a list of receptor and ligand files
     receptor_files = [os.path.join(receptor_folder, file) for file in os.listdir(receptor_folder) if file.endswith('.pdbqt')]
@@ -61,8 +65,8 @@ def main(args):
     total_pairs = len(receptor_files) * len(ligand_files)
     logging.info(f"Number of total pairs: {total_pairs}")
 
-    pairs = [(receptor_file, ligand_file, output_folder,exhaustiveness) for receptor_file in receptor_files for ligand_file in ligand_files]
-    num_processes = cpu_count()
+    pairs = [(receptor_file, ligand_file, output_folder,exhaustiveness,centre,box_size) for receptor_file in receptor_files for ligand_file in ligand_files]
+    num_processes = num_cpus
     number_of_docked_pairs = 0
 
     logging.info(f"Total number of CPUs available in this run: {num_processes}")
@@ -82,6 +86,13 @@ if __name__ == "__main__":
     parser.add_argument("--output_folder", type=str, help="Path to the output folder")
     # exhaustiveness
     parser.add_argument("--exhaustiveness", "-e",type=int, default=32, help="Exhaustiveness parameter for Vina")
+
+    # add the arguments for centre coordinates and box size
+    parser.add_argument("--center", "-c", type=float, nargs=3, default=[5, 2, -15], help="Center coordinates for Vina")
+    parser.add_argument("--box_size", "-b", type=float, nargs=3, default=[20, 20, 20], help="Box size for Vina")
+    
+    # number of CPUs to use
+    parser.add_argument("--num_cpus", "-n", type=int, default=cpu_count(), help="Number of CPUs to use")
     args = parser.parse_args()
 
     main(args)
